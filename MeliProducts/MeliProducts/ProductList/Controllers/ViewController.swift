1b6558668f99f9e28ec44580fc466b002c63364e
Matheus Feola <matheus.feola@concrete.com.br>
2019-06-30 21:45:42 -0300

    Add base url to Plist file

--- MeliProducts/MeliProducts/ProductList/Controllers/ViewController.swift
+++ MeliProducts/MeliProducts/ProductList/Controllers/ViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let service: ProductServiceProtocol
+    
+    init(service: ProductServiceProtocol = ProductService()) {
+        self.service = service
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        self.service = ProductService() // remove later
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        service.productDetail(productId: "MLU445069856") { product in
+            
+            print("I'm selling a: \(product.title)")
+            
+        }
+        
+        service.search("TV") { productList in
+            
+            print("And it costs: \(productList.results[1].price)")
+        }
     }
-
-
 }
 
