7a19889d76af4641909f46f92e84352b12506db1
Matheus Feola <matheus.feola@concrete.com.br>
2019-07-02 20:22:56 -0300

    Picture model

---
+++ MeliProducts/MeliProducts/Service/Product/Model/Picture.swift
@@ -0,0 +1,13 @@
+//
+//  Picture.swift
+//  MeliProducts
+//
+//  Created by matheus.feola on 02/07/19.
+//  Copyright © 2019 matheus.feola. All rights reserved.
+//
+
+import Foundation
+
+struct Picture: Codable {
+    var secure_url: URL
+}
